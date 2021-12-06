1193f6770566b10d427094afbfbe4d2d2cda8df6
Robert <robertwt7@gmail.com>
2021-12-06 22:28:49 +1100

    fix: style and formik-material for mui v5

--- frontend/src/Copyright.js
+++ frontend/src/Copyright.js
@@ -1,14 +1,13 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import MuiLink from '@mui/material/Link';
+import Typography from "@mui/material/Typography";
+import MuiLink from "@mui/material/Link";
 
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <MuiLink color="inherit" href="https://mui.com/">
         Your Website
-      </MuiLink>{' '}
+      </MuiLink>{" "}
       {new Date().getFullYear()}.
     </Typography>
   );
