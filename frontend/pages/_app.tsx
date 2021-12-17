0fdfffe27ca6f6ec41c4902d979d77b696b0e2f8
Robert Tirta <robert.tirtasentana@afterpay.com>
2021-12-18 09:27:09 +1100

    feat: add google oauth provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,6 +8,7 @@ import DateAdapter from "@mui/lab/AdapterDayjs";
 import { LocalizationProvider } from "@mui/lab";
 import type { EmotionCache } from "@emotion/utils";
 import type { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
 import theme from "../src/theme";
 import createEmotionCache from "../src/createEmotionCache";
 import "tailwindcss/tailwind.css";
@@ -19,7 +20,6 @@ export default function MyApp(
   props: AppProps & { emotionCache: EmotionCache | undefined }
 ): ReactNode {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -27,11 +27,13 @@ export default function MyApp(
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={DateAdapter}>
-        <ThemeProvider theme={theme}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
+        <SessionProvider session={props.pageProps?.session}>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </SessionProvider>
       </LocalizationProvider>
     </CacheProvider>
   );
--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
     // ...add more providers here
   ],
