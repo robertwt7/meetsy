1f4d9eee0b9ca89886d42298ee027b90b893fdc2
Robert Tirta <robert.tirtasentana@afterpay.com>
2021-12-18 10:49:14 +1100

    feat: api dependencies, logo

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -10,4 +10,21 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,15 +2,21 @@ import { Button, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { Copyright } from "src";
+import Image from "next/image";
+import logo from "public/images/logo.png";
+import { useSession } from "next-auth/react";
 
 const Index: NextPage = () => {
   const router = useRouter();
   const handleMeet = (): void => {
     void router.push("/meet");
   };
+  const session = useSession();
+
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <div className="flex flex-col items-center">
+        <Image src={logo} alt="logo" width={200} height={200} />
         <Typography variant="h3" fontWeight="semibold" gutterBottom>
           Meetsy
         </Typography>
