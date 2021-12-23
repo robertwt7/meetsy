1b69976f2e0dcad643135875b5073450c856e16e
Robert <robertwt7@gmail.com>
2021-12-23 23:02:04 +1100

    feat: create auth app

---
+++ api/core/core/secrets.example.py
@@ -0,0 +1,2 @@
+DJANGO_SECRET_KEY = "your django secret key"
+JWT_SECRET_KEY = "your jwt signing secret key"
--- api/core/core/settings.py
+++ api/core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "dj_rest_auth",
+    "dj_rest_auth.registration",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
