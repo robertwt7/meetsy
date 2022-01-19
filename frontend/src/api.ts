4640e4960c89c23d2bf528210ba08a5325d43cb2
Robert <robertwt7@gmail.com>
2022-01-19 23:14:07 +1100

    feat: extract backend url from src/env

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,6 @@
-const BACKEND_URL = process.env.BACKEND_API_BASE ?? "";
+import { api } from "src/env";
 
+const { BACKEND_URL } = api;
 export const VERIFY_TOKEN_URL = `${BACKEND_URL}/api/auth/token/verify/`;
 export const REFRESH_TOKEN_URL = `${BACKEND_URL}/api/auth/token/refresh/`;
 export const LOGIN_URL = (provider: string): string =>
