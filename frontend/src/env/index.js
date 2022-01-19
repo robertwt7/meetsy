c75a45c049a125bc841dbbc52268f9590e0a91e6
Robert <robertwt7@gmail.com>
2022-01-19 23:12:58 +1100

    feat: redux toolkit initial setup

---
+++ frontend/src/env/index.js
@@ -0,0 +1,6 @@
+export const api = {
+  BACKEND_URL: process.env.BACKEND_URL ?? "",
+};
+export const app = {
+  DEBUG_MODE: process.env.DEBUG_MODE ?? false,
+};
---
+++ frontend/src/state/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import { app } from "../env";
+
+const { DEBUG_MODE } = app;
+
+export const store = configureStore({
+  reducer: {},
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export type MeetsyAppStore = typeof store;
+
+export const wrapper = createWrapper<MeetsyAppStore>(() => store, {
+  debug: DEBUG_MODE as boolean,
+});
