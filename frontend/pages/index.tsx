24c0fc524dd4a9a3a696e2ba4bb03af5e70a62a9
Robert <robertwt7@gmail.com>
2022-01-22 11:43:32 +1100

    feat: add user calendar to home

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import { Copyright, WelcomePanel } from "src";
+import { Copyright, WelcomePanel, UserCalendar } from "src";
 
 const Index: NextPage = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <WelcomePanel />
+      <UserCalendar />
       <div className="my-8">
         <Copyright />
       </div>
