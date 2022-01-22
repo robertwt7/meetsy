69a068d0d40157b0696fb8535762e372e6f2531a
Robert <robertwt7@gmail.com>
2022-01-23 07:43:46 +1100

    feat: set default auth header

--- frontend/src/component/UserCalendar/UserCalendar.tsx
+++ frontend/src/component/UserCalendar/UserCalendar.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { Typography } from "@mui/material";
 import { useGetEventsQuery } from "src/services/backend";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import format from "date-fns/format";
@@ -7,6 +8,9 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from "date-fns/getDay";
 import enAU from "date-fns/locale/en-AU";
 import { startOfMonth, endOfMonth, formatISO } from "date-fns";
+
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const locales = {
   "en-AU": enAU,
@@ -29,7 +33,10 @@ export const UserCalendar: FunctionComponent = () => {
 
   console.log(data);
   return (
-    <div>
+    <div className="mt-4">
+      <Typography align="center" variant="h5">
+        Your events
+      </Typography>
       <Calendar
         localizer={localizer}
         events={data?.items}
--- frontend/src/services/backend/backend.ts
+++ frontend/src/services/backend/backend.ts
@@ -1,15 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { api } from "src/env";
+import { getSession } from "next-auth/react";
 import { EventsRequest, EventsResponse } from "./model";
 
 export const backendApi = createApi({
   reducerPath: "backendApi",
-  baseQuery: fetchBaseQuery({ baseUrl: `${api.BACKEND_URL}/api` }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${api.BACKEND_URL}/api`,
+    prepareHeaders: async (headers) => {
+      const session = await getSession();
+      console.log("result of getsession: ", session);
+
+      const token = session?.accessToken;
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token as string}`);
+      }
+
+      return headers;
+    },
+  }),
   tagTypes: ["Events"],
   endpoints: (builder) => ({
     getEvents: builder.query<EventsResponse, EventsRequest>({
       query: (body) => ({
-        url: "/events",
+        url: "/events/",
         method: "POST",
         data: body,
       }),
