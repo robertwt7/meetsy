102cd40596b2fcf87bf56ae531b42b8e6a12cc81
Robert <robertwt7@gmail.com>
2022-02-26 11:05:42 +1100

    feat: formik select to be used in form

--- frontend/src/component/AcceptForm/AcceptForm.tsx
+++ frontend/src/component/AcceptForm/AcceptForm.tsx
@@ -11,40 +11,38 @@ export const AcceptForm: FunctionComponent<AcceptFormProps> = ({ url }) => {
   const { data, isFetching, isError, error } = useGetInvitedEventQuery(url);
 
   // TODO: check if there is any error in the query
-  return (
-    !isFetching && (
-      <div className="flex flex-row w-full">
-        <Paper className="w-full">
-          <div className="flex flex-row items-stretch">
-            <div className="w-1/4">
-              <div className="flex flex-row space-x-4 m-4 mb-2 items-center">
-                <PersonIcon />
-                <Typography variant="h5">
-                  {data?.user?.first_name} {data?.user?.last_name}
-                </Typography>
-              </div>
-              <Typography variant="h6" className="ml-4">
-                {data?.user?.email}
-              </Typography>
-              <Typography variant="h4" className="m-4 mb-2">
-                {data?.name}
-              </Typography>
-              <Typography variant="body1" className="ml-4 m-2">
-                {data?.location}
-              </Typography>
-              <Typography variant="body2" className="ml-4 m-2">
-                {data?.notes}
+  return !isFetching ? (
+    <div className="flex flex-row w-full">
+      <Paper className="w-full">
+        <div className="flex flex-row items-stretch">
+          <div className="w-1/4">
+            <div className="flex flex-row space-x-4 m-4 mb-2 items-center">
+              <PersonIcon />
+              <Typography variant="h5">
+                {data?.user?.first_name} {data?.user?.last_name}
               </Typography>
             </div>
-            <div className="w-3/4 border-l p-4 border-gray-300">
-              <UserCalendar
-                selectable={false}
-                availableDates={mapDatesToCalendarObject(data?.available_dates)}
-              />
-            </div>
+            <Typography variant="h6" className="ml-4">
+              {data?.user?.email}
+            </Typography>
+            <Typography variant="h4" className="m-4 mb-2">
+              {data?.name}
+            </Typography>
+            <Typography variant="body1" className="ml-4 m-2">
+              {data?.location}
+            </Typography>
+            <Typography variant="body2" className="ml-4 m-2">
+              {data?.notes}
+            </Typography>
           </div>
-        </Paper>
-      </div>
-    )
-  );
+          <div className="w-3/4 border-l p-4 border-gray-300">
+            <UserCalendar
+              selectable={false}
+              availableDates={mapDatesToCalendarObject(data?.available_dates)}
+            />
+          </div>
+        </div>
+      </Paper>
+    </div>
+  ) : null;
 };
