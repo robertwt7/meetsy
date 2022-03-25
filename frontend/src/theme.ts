d0fef4f2a0d6be61ccb7c5e06621e0e33a62c869
Robert <robertwt7@gmail.com>
2022-03-25 11:21:48 +1100

    feat: match tailwind and MUI breakpoints

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,8 +2,29 @@ import { createTheme } from "@mui/material/styles";
 import type {} from "@mui/lab/themeAugmentation";
 import { red } from "@mui/material/colors";
 
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xs: true;
+    sm: true;
+    md: true;
+    lg: true;
+    xl: true;
+    "2xl": true;
+  }
+}
+
 // Create a theme instance.
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      "2xl": 1536,
+    },
+  },
   palette: {
     primary: {
       main: "#0D47A1",
