854f71b343bdc75de5657235e4bc8f28c4ed32d0
Robert <robertwt7@gmail.com>
2022-04-06 23:12:31 +1000

    fix: static files

--- api/app/core/settings.py
+++ api/app/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .secrets import (
     DJANGO_SECRET_KEY,
@@ -217,6 +218,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
