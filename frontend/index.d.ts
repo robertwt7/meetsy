4da79f54f030816586c632133299c6d63c1802db
Robert <robertwt7@gmail.com>
2022-04-21 21:47:47 +1000

    fix: unused stuff

--- frontend/index.d.ts
+++
@@ -1,8 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-
-declare global {
-  interface Window {
-    // TODO: replace this with a more specific type based on usage
-    dataLayer: any[];
-  }
-}
