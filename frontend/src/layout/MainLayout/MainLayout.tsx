e73ea70fc70ccb7a4534f22a3f9853e556aeb662
Robert <robertwt7@gmail.com>
2022-04-23 19:10:24 +1000

    fix: logo to be clicked

--- frontend/src/layout/MainLayout/MainLayout.tsx
+++ frontend/src/layout/MainLayout/MainLayout.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { signOut, useSession } from "next-auth/react";
 import logo from "public/images/meetsy_logo.png";
 import { useRouter } from "next/router";
-import { Navigation } from "../../component";
+import { Link, Navigation } from "../../component";
 
 const hiddenNavigation = ["/invite"];
 
@@ -34,12 +34,16 @@ export const MainLayout: FunctionComponent = ({ children }) => {
       <div className="ml-auto mr-auto flex min-h-screen w-full flex-col px-8 md:w-3/5">
         {isUnauthenticated ? (
           <div className="mt-8 w-1/6 self-center">
-            <Image src={logo} alt="logo" layout="responsive" />
+            <Link href="/">
+              <Image src={logo} alt="logo" layout="responsive" />
+            </Link>
           </div>
         ) : (
           <div className={navigationClassName}>
             <div className="w-1/6">
-              <Image src={logo} alt="logo" layout="responsive" />
+              <Link href="/">
+                <Image src={logo} alt="logo" layout="responsive" />
+              </Link>
             </div>
             <div className="ml-auto mr-0 flex flex-row space-x-8">
               <Navigation
