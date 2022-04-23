04042d4cf32f188fc5209b97b93814a3f20c3588
Robert <robertwt7@gmail.com>
2022-04-23 20:55:35 +1000

    feat: links in footer

--- frontend/src/component/Footer/Footer.tsx
+++ frontend/src/component/Footer/Footer.tsx
@@ -1,6 +1,22 @@
 import { FunctionComponent } from "react";
+import { Route } from "src/config/router";
+import { NavigationList, INavigationItemProps } from "../Navigation";
 import { Copyright } from "../Copyright";
 
 export const Footer: FunctionComponent = () => {
-  return <Copyright />;
+  const items: INavigationItemProps[] = [
+    {
+      text: "Privacy Policy",
+      testId: "privacypolicy",
+      route: Route.PRIVACY_POLICY,
+    },
+  ];
+  return (
+    <div className="flex flex-col">
+      <Copyright />
+      <div className="flex flex-row justify-center">
+        <NavigationList items={items} textVariant="body1" />
+      </div>
+    </div>
+  );
 };
