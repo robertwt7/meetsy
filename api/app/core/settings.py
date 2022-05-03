871a42be802a1fe72feaae6a0d38284233f28ab4
Robert <robertwt7@gmail.com>
2022-05-03 21:14:13 +1000

    fix: refresh token lifetime

--- api/app/core/settings.py
+++ api/app/core/settings.py
@@ -125,7 +125,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
