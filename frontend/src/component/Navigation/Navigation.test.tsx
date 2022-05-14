00e153f30029c233dda05db407e09e65a6966579
Robert <robertwt7@gmail.com>
2022-05-14 11:28:41 +1000

    test: snapshot and mocks

--- frontend/src/component/Navigation/Navigation.test.tsx
+++ frontend/src/component/Navigation/Navigation.test.tsx
@@ -5,6 +5,15 @@ import { Navigation } from "./Navigation";
 
 const mocks = {
   useMediaQuery: jest.fn().mockReturnValue(true),
+  useSession: jest.fn().mockReturnValue({
+    sessionData: {
+      user: {
+        name: "Test user",
+        email: "test@gmail.com",
+      },
+    },
+    status: "authenticated",
+  }),
 };
 
 jest.mock("next/router", () => ({
@@ -20,6 +29,10 @@ jest.mock("@mui/material", () => {
     useMediaQuery: () => mocks.useMediaQuery(),
   };
 });
+
+jest.mock("next-auth/react", () => ({
+  useSession: () => mocks.useSession(),
+}));
 
 describe("Navigation component", () => {
   test("should match snapshot", () => {
