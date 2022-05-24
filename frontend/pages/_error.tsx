3ff5efad8bc704ee5b293f5bc853b0d8fcf91e54
richardpur <32887797+richardpur@users.noreply.github.com>
2022-05-24 21:56:17 +1000

    Added link to home for error 404 page

--- frontend/pages/_error.tsx
+++ frontend/pages/_error.tsx
@@ -1,10 +1,24 @@
 import { NextPage } from "next";
+import { Typography } from "@mui/material";
+import { common } from "@mui/material/colors";
+import { Link } from "src/component/Link";
 import ErrorImage from "src/assets/404_error.svg";
 import Image from "next/image";
 
 const Error: NextPage = () => {
   return (
     <div className="m-auto w-4/5 max-w-screen-md">
+      <Typography
+        variant="h4"
+        align="center"
+        color={common.black}
+        font-weight="bold"
+      >
+        Page Not Found!
+      </Typography>
+      <Typography className="mt-0.5 mb-3" variant="h6" align="center">
+        <Link href="/">Back to Home</Link>
+      </Typography>
       <Image src={ErrorImage} alt="error" layout="responsive" />
     </div>
   );
